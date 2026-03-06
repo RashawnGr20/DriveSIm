7477e1340492795bbe84b745a6f5ed343887bbd8
Rashawn GR20 <rashawngrantrhooms@gmail.com>
2026-03-06 15:01:49 -0500

    we making stridess

--- sim/scenes.py
+++ sim/scenes.py
@@ -3,61 +3,111 @@ from feedback import feedBackEngine
 feedback = feedBackEngine()
 class scene :
 
-    def __init__(self, scenes, sequence, min_glance):
+    def __init__(self):
 
         self.scenes = { 
 
-            'left_lane_change' :  {
-                'sequence': ['TOP MIRROR', 'LEFT MIRROR', 'LEFT BLINDSPOT'], 
-                'min_glance':  10
-            },
+            'left_lane_change' : SequenceScene(
+                10,
+                ['TOP MIRROR', 'LEFT MIRROR', 'LEFT BLINDSPOT'] 
+            ),
 
-            'right_lane_change': { 
-                
-                'sequence': ['TOP MIRROR', 'RIGHT MIRROR', 'RIGHT BLINDSPOT'], 
-                'min_glance': 8                  
-            
-            }
-
+            '4Way_left_turn' : coverageScene(
+                10,
+                ['FORWARD', 'LEFT-MIRROR', 'RIGHT-MIRROR']
+            )
         }
 
-        self.curr_step = 0
-        self.last_pose_counter = 0
-        self.last_pose = None
-        self.step_lock = False 
-        self.glance_data = []
-
-    def evaluation(self, selected_scene, pose_counter, fps, pose) :
-        
-        scene = self.scenes[selected_scene]
-        expected_sequence = scene['sequence']
-        min_glance = scene['min_glance']
-        
-        
-        
-        if self.curr_step >= len(expected_sequence) :
-            pass 
+    
+    def get_scene(self, selected_scene) :
+         
+        if selected_scene in self.scenes :
+            return self.scenes[selected_scene]
         else :
-            expected_pose = expected_sequence[self.curr_step]
-        
-        if self.last_pose and pose != self.last_pose:
-            duration_frames = self.last_pose_counter
-            duration_seconds = duration_frames / fps
-            self.glance_data.append((self.last_pose, duration_seconds))
-            self.step_lock = False
+             print('Scene does not exist') 
+             return None
 
-        if pose == expected_pose and pose_counter >= min_glance and self.step_lock == False:
-            self.curr_step += 1
-            self.step_lock = True 
+
+    
+    def evaluation(self, pose_counter, fps, pose) :
+      pass   
         
-        elif pose in expected_sequence and pose != expected_pose and pose_counter >= min_glance :
+    
+    
+class SequenceScene :
+
+        def __init__(self, min_glance, expected_sequence):
+            self.expected_sequence = expected_sequence
+            self.min_glance = min_glance 
             self.curr_step = 0
-        
-        else : 
-            pass 
+            self.observations = []
+            self.step_lock = False
+            self.last_pose = None
 
-        self.last_pose = pose
-        self.last_pose_counter = pose_counter
+        def evaluate(self, pose, pose_counter) :
+
+            if self.curr_step >= len(self.expected_sequence) :
+                    return self.observations
+            else :
+                expected_pose = self.expected_sequence[self.curr_step]
+       
+            if self.last_pose and pose != self.last_pose:
+                self.step_lock = False 
+        
+
+            if pose == expected_pose and pose_counter >= self.min_glance and self.step_lock == False:
+                self.curr_step += 1
+                self.step_lock = True
+                self.observations.append(pose) 
+        
+            elif pose in self.expected_sequence and pose != expected_pose and pose_counter >= self.min_glance and self.step.lock == False :
+                self.curr_step += 1
+                self.observations.append(pose)
+        
+            else : 
+                pass 
+
+            self.last_pose = pose
+
+    
+class coverageScene :
+
+        def __init__(self, min_glance, expected_sequence):
+            
+            self.min_glance = min_glance 
+            self.required_zones = set(expected_sequence)
+            self.checked_zones = set()
+            self.step_lock = False
+            self.last_pose = None
+        
+        def evaluate(self, pose, pose_counter) :
+
+
+            if self.checked_zones == self.required_zones :
+                return self.checked_zones 
+            
+            if self.last_pose and pose != self.last_pose:
+                self.step_lock = False 
+        
+            if pose in self.required_zones and self.step_lock == False and pose_counter >= self.min_glance:
+                self.checked_zones.add(pose)
+                self.step_lock = True
+            
+            self.last_pose = pose
+
+
+            return self.checked_zones
+            
+
+
+
+
+
+            
+
+
+
+         
 
 
 
