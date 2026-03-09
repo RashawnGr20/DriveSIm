171479e1757e0572b318c5187e12cb60100ba455
RashawnGr20 <ronrundy2@gmail.com>
2026-03-09 18:01:21 -0400

    its cooked

--- sim/UI.py
+++ sim/UI.py
@@ -1,8 +1,16 @@
 import pygame 
+import os 
 
 class UI :
   
   def __init__(self, screen, screen_width, screen_height, ):
+    
+    
+    base_dir = os.path.dirname(__file__)
+    
+    small_font_path = os.path.join(base_dir, "fonts", "Inter_18pt-ExtraLight.ttf")
+    
+    large_font_path = os.path.join(base_dir, "fonts", "Inter_28pt-SemiBold.ttf")
      
     self.W = screen_width
     self.H = screen_height
@@ -20,16 +28,15 @@ class UI :
     }
     
     self.fonts = {
-      #"small": pygame.font.Font("fonts/inter.ttf", 16),
-      #"medium": pygame.font.Font("fonts/inter.ttf", 22), 
-      #"large": pygame.font.Font("fonts/inter.ttf", 32)
-    
+      "small": pygame.font.Font(small_font_path, 16),
+      "medium": pygame.font.Font(small_font_path, 22), 
+      "large": pygame.font.Font(large_font_path, 32)
+  
     }
-    
-    self.font_med = pygame.font.SysFont("Arial", 24)
 
   
   def draw(self, pose) :
+    
     self.draw_running_ui(pose)
   
   
@@ -45,18 +52,47 @@ class UI :
   
   
   def draw_bottom_panel(self, panel_color, pose, text_color) :
-    panel_y = self.H - self.panel_size
-    pygame.draw.rect(
-      self.screen, 
-      panel_color,
-      (0, panel_y, self.W, self.panel_size)
-    )
-  
-    text_surface = self.font_med.render(f"POSE: {pose}", True, text_color,)
-    self.screen.blit(text_surface, (20, panel_y + 20)) 
+   
+    panel_y = self.H - self.panel_size 
     
-    pygame.draw.line(self.screen, (80,80,90), (0, panel_y), (self.W, panel_y), 2)
+    panel_surface = pygame.Surface((self.W, self.panel_size), pygame.SRCALPHA)
+    panel_surface.set_alpha(230)
+    panel_surface.fill(panel_color)
+    
+    self.screen.blit(panel_surface, (0, panel_y))
+    
+    pygame.draw.line(
+      self.screen, 
+      self.colors["divider"],
+      (250, panel_y + 10),
+      (250, panel_y + self.H - 10),
+      1
+    )
+    
+    pygame.draw.line(
+      self.screen, 
+      self.colors["accent"], 
+      (0, panel_y),
+      (self.W, panel_y),
+      2
+    )
+    
+    label = self.fonts["small"].render("CURRENT POSE", True, self.colors['divider'])
+    self.screen.blit(label, (20, panel_y + 10)) 
+    
+    pose_text = self.fonts["large"].render(pose, True, self.colors["accent"])
+    self.screen.blit(pose_text, (20, panel_y + 40))
+    
   
+    progress_label = self.fonts["small"].render(
+      "OBSERVATIONS PROGRESS", 
+      True,
+      self.colors["divider"]
+    )
+    
+    self.screen.blit(progress_label, (270, panel_y + 10))
+    
+    
   
   def draw_progress(self) :
     pass 
